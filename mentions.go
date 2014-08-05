96e0af4657cdb18206f68b8cd413679774407ac2
Kaleb Elwert <kelwert@mtu.edu>
2014-08-05 18:55:50 +0000

    Add a ping response

--- mentions.go
+++ mentions.go
@@ -4,6 +4,8 @@ import "bitbucket.org/belak/irc"
 
 func MentionsHandler(c *irc.Client, e *irc.Event) {
 	switch e.Trailing() {
+	case "ping":
+		c.MentionReply(e, "pong")
 	case "scoobysnack", "scooby snack":
 		c.Reply(e, "Scooby Dooby Doo!")
 	case "botsnack", "bot snack":
