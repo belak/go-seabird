46a80a12b782b797bf935b5232c3a528eace59ac
Nathan Scowcroft <ns@endrake.com>
2014-08-06 01:16:17 -0400

    check basic say format

--- say.go
+++ say.go
@@ -8,5 +8,10 @@ import (
 
 func SayHandler(c *irc.Client, e *irc.Event) {
 	p := strings.SplitN(e.Trailing(), " ", 2)
+	if len(p) != 2 {
+		c.MentionReply(e, "usage: !say <channel> <msg>")
+		return
+	}
+
 	c.Writef("PRIVMSG %s :%s", p[0], p[1])
 }
