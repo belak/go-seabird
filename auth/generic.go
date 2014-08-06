f7fb789e9b54ccd9d917ea37a8fb2d1e41ae57d8
Nathan Scowcroft <ns@endrake.com>
2014-08-05 21:57:20 -0400

    Add CheckAuth

--- auth/generic.go
+++ auth/generic.go
@@ -1,27 +1,10 @@
-// +build ignore
-
 package auth
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"hash"
-	"io"
-	"strings"
-
-	"github.com/thoj/go-ircevent"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
+	"bitbucket.org/belak/irc"
 )
-
-// TODO: Log actions
-// TODO: Use trim a bit
-
-func init() {
-	seabird.RegisterAuthPlugin("generic", NewGenericAuthPlugin)
-}
 
 type GenericAccount struct {
 	Id    bson.ObjectId `bson:"_id"`
@@ -29,222 +12,123 @@ type GenericAccount struct {
 	Perms []string      `bson:"perms,omitempty"`
 }
 
-type GenericAuthPluginConfig struct {
-	Salt string
+type User struct {
+	CurrentNick string
+	Account string
+	Channels []string
 }
 
-type GenericAuthPlugin struct {
-	Bot    *seabird.Bot
+type GenericAuth struct {
+	Client *irc.Client
 	C      *mgo.Collection
-	Config GenericAuthPluginConfig
+	Users  map[string]*User
+	salt   string
 }
 
-func NewGenericAuthPlugin(b *seabird.Bot, c json.RawMessage) seabird.AuthPlugin {
-	p := &GenericAuthPlugin{Bot: b, C: b.DB.C("generic_auth_accounts")}
-	b.RegisterFunction("login", p.Login)
-	b.RegisterFunction("logout", p.Logout)
-	b.RegisterFunction("register", p.Register)
-	b.RegisterFunction("addperm", p.AddPerm)
-	b.RegisterFunction("delperm", p.DelPerm)
-	b.RegisterFunction("whois", p.Whois)
-	b.RegisterCallback("JOIN", p.join)
+func NewGenericAuth(c *irc.Client, db *mgo.Database, salt string) *GenericAuth{
+	p := &GenericAuth{Client: c, C: db.C("generic_auth_accounts"), salt: salt}
+	p.trackUsers()
 
-	json.Unmarshal(c, &p.Config)
 
 	return p
 }
 
-func (p *GenericAuthPlugin) GetHash() hash.Hash {
-	h := md5.New()
-	io.WriteString(h, p.Config.Salt)
+func (au *GenericAuth) CheckPerm(p string, h irc.Handler) irc.Handler {
 	return h
 }
 
-func (p *GenericAuthPlugin) join(e *irc.Event) {
-	if e.Nick == p.Bot.Conn.GetNick() {
-		p.Bot.Conn.SendRawf("WHO %s", e.Message())
-	}
+func (au *GenericAuth) CheckPermFunc(p string, f irc.HandlerFunc) irc.HandlerFunc{
+	return f
 }
 
-func (p *GenericAuthPlugin) Whois(e *irc.Event) {
-	u := p.Bot.GetUser(e.Message())
-	if u.Account != "" {
-		p.Bot.MentionReply(e, "%s is logged in as %s", u.CurrentNick, u.Account)
-	} else {
-		p.Bot.MentionReply(e, "%s is not logged in", u.CurrentNick)
+// user tracking utilities
+
+func (au *GenericAuth) GetUser(nick string) *User{
+	u, ok := au.Users[nick]
+	if !ok {
+		u = &User{CurrentNick: nick}
 	}
+
+	return u
 }
 
-func (p *GenericAuthPlugin) Login(e *irc.Event) {
-	// TODO: Don't let them login in a channel
-	u := p.Bot.GetUser(e.Nick)
-	if u.Account != "" {
-		p.Bot.MentionReply(e, "you are already logged in")
-		return
-	}
+func (au *GenericAuth) addChannelToNick(c, n string) {
+	u := au.GetUser(n)
 
-	args := strings.SplitN(e.Message(), " ", 2)
-	if len(args) != 2 {
-		p.Bot.MentionReply(e, "usage: !login username password")
-		return
-	}
-
-	h := p.GetHash()
-	io.WriteString(h, args[1])
-
-	c, err := p.C.Find(bson.M{
-		"name":     args[0],
-		"password": hex.EncodeToString(h.Sum(nil)),
-	}).Count()
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if c > 0 {
-		u.Account = args[0]
-		p.Bot.MentionReply(e, "you are now logged in as %s", args[0])
-	} else {
-		p.Bot.MentionReply(e, "login failed")
-	}
-}
-
-func (p *GenericAuthPlugin) Logout(e *irc.Event) {
-	u := p.Bot.GetUser(e.Nick)
-	if u.Account == "" {
-		p.Bot.MentionReply(e, "you are not logged in")
-		return
-	}
-
-	u.Account = ""
-
-	p.Bot.MentionReply(e, "you have been logged out")
-}
-
-func (p *GenericAuthPlugin) Register(e *irc.Event) {
-	u := p.Bot.GetUser(e.Nick)
-	if u.Account != "" {
-		p.Bot.MentionReply(e, "you are already logged in")
-		return
-	}
-
-	args := strings.SplitN(e.Message(), " ", 2)
-	if len(args) < 2 {
-		p.Bot.MentionReply(e, "usage: !register username password")
-		return
-	}
-
-	c, err := p.C.Find(bson.M{
-		"name": args[0],
-	}).Count()
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if c > 0 {
-		p.Bot.MentionReply(e, "there is already a user with that name")
-		return
-	}
-
-	h := p.GetHash()
-	io.WriteString(h, args[1])
-
-	err = p.C.Insert(bson.M{
-		"name":     args[0],
-		"password": hex.EncodeToString(h.Sum(nil)),
-	})
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p.Bot.MentionReply(e, "you have been registered")
-}
-
-func (p *GenericAuthPlugin) AddPerm(e *irc.Event) {
-	u := p.Bot.GetUser(e.Nick)
-	if !p.UserCan(u, "admin") && !p.UserCan(u, "generic_auth.addperm") {
-		p.Bot.MentionReply(e, "you don't have permission to do that")
-		return
-	}
-
-	args := strings.SplitN(e.Message(), " ", 2)
-	if len(args) < 2 {
-		p.Bot.MentionReply(e, "usage: !addperm username perm")
-		return
-	}
-
-	a := GenericAccount{}
-	err := p.C.Find(bson.M{"name": args[0]}).One(&a)
-	if err != nil {
-		// NOTE: This may be another error, but I doubt it
-		p.Bot.MentionReply(e, "account %s does not exist", args[0])
-		return
-	}
-
-	for _, v := range a.Perms {
-		if v == args[1] {
-			p.Bot.MentionReply(e, "%s already has perm %s", args[0], args[1])
+	for i := 0; i < len(u.Channels); i++ {
+		if u.Channels[i] == c {
 			return
 		}
 	}
 
-	err = p.C.UpdateId(a.Id, bson.M{"$push": bson.M{"perms": args[1]}})
-	if err != nil {
-		// NOTE: This may be another error, but I doubt it
-		p.Bot.MentionReply(e, "account %s does not exist", args[0])
-		return
-	}
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p.Bot.MentionReply(e, "added %s perm to %s", args[1], args[0])
+	u.Channels = append(u.Channels, c)
+	au.Users[n] = u
 }
 
-func (p *GenericAuthPlugin) DelPerm(e *irc.Event) {
-	u := p.Bot.GetUser(e.Nick)
-	if !p.UserCan(u, "admin") && !p.UserCan(u, "generic_auth.delperm") {
-		p.Bot.MentionReply(e, "you don't have permission to do that")
-		return
+func (au *GenericAuth) removeChannelFromUser(c string, u *User) {
+	for i := 0; i < len(u.Channels); i++ {
+		if u.Channels[i] == c {
+			// Swap with last element and shrink slice
+			u.Channels[i] = u.Channels[len(u.Channels)-1]
+			u.Channels = u.Channels[:len(u.Channels)-1]
+			break
+		}
 	}
 
-	args := strings.SplitN(e.Message(), " ", 2)
-	if len(args) < 2 {
-		p.Bot.MentionReply(e, "usage: !delperm username perm")
-		return
+	if len(u.Channels) == 0 {
+		// Removing user
+		delete(au.Users, u.CurrentNick)
 	}
-
-	err := p.C.Update(bson.M{"name": args[0]}, bson.M{"$pull": bson.M{"perms": args[1]}})
-	if err != nil {
-		// NOTE: This may be another error, but I doubt it
-		p.Bot.MentionReply(e, "account %s does not exist", args[0])
-		return
-	}
-
-	p.Bot.MentionReply(e, "removed %s perm from %s", args[1], args[0])
 }
 
-func (p *GenericAuthPlugin) UserCan(u *seabird.User, perm string) bool {
-	if u.Account == "" {
-		return false
-	}
+// user tracking
 
-	c, err := p.C.Find(bson.M{
-		"name":  u.Account,
-		"perms": perm,
-	}).Count()
-
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	return c > 0
+func (au *GenericAuth) connectHandler(c *irc.Client, e *irc.Event) {
+	au.Users = make(map[string]*User)
 }
+
+func (au *GenericAuth) joinHandler(c *irc.Client, e *irc.Event) {
+	if e.Identity.Nick != c.CurrentNick() {
+		au.addChannelToNick(e.Args[0], e.Identity.Nick)
+	} else {
+		for _, user := range au.Users {
+			au.removeChannelFromUser(e.Args[0], user)
+		}
+	}
+}
+
+func (au *GenericAuth) nickHandler(c *irc.Client, e *irc.Event) {
+	u := au.GetUser(e.Identity.Nick)
+	if len(u.Channels) == 0 {
+		return
+	}
+
+	u.CurrentNick = e.Args[1]
+	delete(au.Users, e.Identity.Nick)
+	au.Users[u.CurrentNick] = u
+}
+
+func (au *GenericAuth) partHandler(c *irc.Client, e *irc.Event) {
+	if e.Identity.Nick != c.CurrentNick() {
+		if u, ok := au.Users[e.Identity.Nick]; ok {
+			au.removeChannelFromUser(e.Args[0], u)
+		}
+	} else {
+		for _, u := range au.Users {
+			au.removeChannelFromUser(e.Args[0], u)
+		}
+	}
+}
+
+func (au *GenericAuth) quitHandler(c *irc.Client, e *irc.Event) {
+	// TODO implement this
+}
+
+func (au *GenericAuth) trackUsers() {
+	au.Client.EventFunc("001",  au.connectHandler)
+	au.Client.EventFunc("JOIN", au.joinHandler)
+	au.Client.EventFunc("NICK", au.nickHandler)
+	au.Client.EventFunc("PART", au.partHandler)
+	au.Client.EventFunc("QUIT", au.quitHandler)
+}
+
