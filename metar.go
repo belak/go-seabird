a2ccc4622589f530b0caff17932c4a33586416c9
Kaleb Elwert <kelwert@atlassian.com>
2014-09-30 16:54:01 -0700

    Initial metar plugin

---
+++ metar.go
@@ -0,0 +1,74 @@
+package seabird
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"strings"
+	"unicode"
+
+	"bitbucket.org/belak/irc"
+	"bitbucket.org/belak/seabird/bot"
+)
+
+func init() {
+	bot.RegisterPlugin("metar", NewMetarPlugin)
+}
+
+func metar(code string) string {
+	for _, letter := range code {
+		if !unicode.IsDigit(letter) && !unicode.IsLetter(letter) {
+			return "Not a valid airport code"
+		}
+	}
+
+	resp, err := http.Get(fmt.Sprintf("http://weather.noaa.gov/pub/data/observations/metar/stations/%s.TXT", code))
+	if err != nil {
+		return "NOAA appears to be down"
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return "Station does not exist"
+	}
+
+	in := bufio.NewReader(resp.Body)
+	for {
+		line, err := in.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		if strings.HasPrefix(line, code+" ") {
+			return strings.TrimSpace(line)
+		}
+	}
+	return "No results"
+}
+
+type MetarPlugin struct{}
+
+func NewMetarPlugin(b *bot.Bot) (bot.Plugin, error) {
+	p := &MetarPlugin{}
+	err := p.Reload(b)
+	if err != nil {
+		return nil, err
+	}
+
+	b.Command("metar", "[airport code]", p.Metar)
+
+	return p, nil
+}
+
+func (p *MetarPlugin) Reload(b *bot.Bot) error {
+	// Noop
+	return nil
+}
+
+func (p *MetarPlugin) Metar(b *bot.Bot, e *irc.Event) {
+	if !e.FromChannel() {
+		return
+	}
+
+	b.MentionReply(e, "%s", metar(e.Trailing()))
+}
