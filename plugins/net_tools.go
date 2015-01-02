5716d7367ec92b67e880312ec0e63c62d119a3aa
Jay Vana <jsvana@mtu.edu>
2015-01-01 18:21:28 -0800

    Make calls async

--- plugins/net_tools.go
+++ plugins/net_tools.go
@@ -23,40 +23,46 @@ func NewNetToolsPlugin(m *mux.CommandMux) error {
 }
 
 func Dig(c *irc.Client, e *irc.Event) {
-	if e.Trailing() == "" {
-		c.MentionReply(e, "Domain required")
-		return
-	}
+	go func() {
+		if e.Trailing() == "" {
+			c.MentionReply(e, "Domain required")
+			return
+		}
 
-	addrs, err := net.LookupHost(e.Trailing())
-	if err != nil {
-		c.MentionReply(e, err.Error())
-	}
+		addrs, err := net.LookupHost(e.Trailing())
+		if err != nil {
+			c.MentionReply(e, err.Error())
+		}
 
-	c.MentionReply(e, addrs[0])
+		c.MentionReply(e, addrs[0])
+	}()
 }
 
 func Ping(c *irc.Client, e *irc.Event) {
-	if e.Trailing() == "" {
-		c.MentionReply(e, "Host required")
-		return
-	}
+	go func() {
+		if e.Trailing() == "" {
+			c.MentionReply(e, "Host required")
+			return
+		}
 
-	out, err := exec.Command("ping", "-c1", e.Trailing()).Output()
-	if err != nil {
-		c.MentionReply(e, err.Error())
-		return
-	}
+		out, err := exec.Command("ping", "-c1", e.Trailing()).Output()
+		if err != nil {
+			c.MentionReply(e, err.Error())
+			return
+		}
 
-	result := strings.Split(string(out), "\n")[1]
-	c.MentionReply(e, result)
+		result := strings.Split(string(out), "\n")[1]
+		c.MentionReply(e, result)
+	}()
 }
 
 func DnsCheck(c *irc.Client, e *irc.Event) {
-	if e.Trailing() == "" {
-		c.MentionReply(e, "Domain required")
-		return
-	}
+	go func() {
+		if e.Trailing() == "" {
+			c.MentionReply(e, "Domain required")
+			return
+		}
 
-	c.MentionReply(e, "https://www.whatsmydns.net/#A/" + e.Trailing())
+		c.MentionReply(e, "https://www.whatsmydns.net/#A/" + e.Trailing())
+	}()
 }
