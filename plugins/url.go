1ac18c8942b250acd40d5527884b5ef5ae8e2068
Jay Vana <jsvana@mtu.edu>
2015-01-03 13:56:07 -0800

    Add Twitter check

--- plugins/url.go
+++ plugins/url.go
@@ -14,10 +14,15 @@ import (
 	"github.com/belak/irc"
 	"github.com/belak/seabird/bot"
 	"github.com/belak/seabird/mux"
+	"github.com/ChimeraCoder/anaconda"
 )
 
 func init() {
 	bot.RegisterPlugin("url", NewURLPlugin)
+}
+
+type URLPlugin struct {
+	Twitter *anaconda.TwitterApi
 }
 
 // NOTE: This isn't perfect in any sense of the word, but it's pretty close
@@ -33,8 +38,10 @@ var client = &http.Client{
 	Timeout: 5 * time.Second,
 }
 
-func NewURLPlugin(bm *irc.BasicMux, cm *mux.CommandMux) error {
-	bm.Event("PRIVMSG", URLTitle)
+func NewURLPlugin(bm *irc.BasicMux, cm *mux.CommandMux, twitter *anaconda.TwitterApi) error {
+	p := &URLPlugin{twitter}
+
+	bm.Event("PRIVMSG", p.URLTitle)
 
 	cm.Event("down", IsItDown, &mux.HelpInfo{
 		"<website>",
@@ -44,58 +51,89 @@ func NewURLPlugin(bm *irc.BasicMux, cm *mux.CommandMux) error {
 	return nil
 }
 
-func URLTitle(c *irc.Client, e *irc.Event) {
+func (p *URLPlugin) URLTitle(c *irc.Client, e *irc.Event) {
 	for _, url := range urlRegex.FindAllString(e.Trailing(), -1) {
 		go func(url string) {
-			r, err := client.Get(url)
-			if err != nil {
-				return
-			}
-			defer r.Body.Close()
-
-			if r.StatusCode != 200 {
-				return
-			}
-
-			// We search the first 1K and if a title isn't in there, we deal with it
-			z, err := html.Parse(io.LimitReader(r.Body, 1024*1024))
-			if err != nil {
-				return
-			}
-
-			// DFS that searches the tree for any node named title then
-			// returns the data of that node's first child
-			var f func(*html.Node) (string, bool)
-			f = func(n *html.Node) (string, bool) {
-				// If it's an element and it's a title node, look for a child
-				if n.Type == html.ElementNode && n.Data == "title" {
-					if n.FirstChild != nil {
-						t := n.FirstChild.Data
-						t = titleRegex.ReplaceAllString(t, " ")
-						t = strings.TrimSpace(t)
-
-						if t != "" {
-							return t, true
-						} else {
-							return "", false
-						}
-					}
+			var msg string
+			if isTwitter(url) {
+				msg = p.getTwitterString(url)
+			} else {
+				title := getTitle(url)
+				if title == "" {
+					msg = "No title found"
+				} else {
+					msg = "Title: " + getTitle(url)
 				}
-
-				// Loop through all nodes and try recursing
-				for c := n.FirstChild; c != nil; c = c.NextSibling {
-					if str, ok := f(c); ok {
-						return str, true
-					}
-				}
-
-				return "", false
 			}
 
-			if str, ok := f(z); ok {
-				c.Reply(e, "Title: %s", str)
-			}
+			c.Reply(e, "%s", msg)
 		}(url)
+	}
+}
+
+func isTwitter(url string) bool {
+	return strings.HasPrefix(url, "https://twitter.com/")
+}
+
+func (p *URLPlugin) getTwitterString(url string) string {
+	statusRegex := regexp.MustCompile(`https://twitter.com/.*?/status/(.+)`)
+	id := statusRegex.FindStringSubmatch(url)
+	if len(id) == 0 {
+		return "No tweet found"
+	}
+
+	return id[0]
+}
+
+func getTitle(url string) string {
+	r, err := client.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != 200 {
+		return ""
+	}
+
+	// We search the first 1K and if a title isn't in there, we deal with it
+	z, err := html.Parse(io.LimitReader(r.Body, 1024*1024))
+	if err != nil {
+		return ""
+	}
+
+	// DFS that searches the tree for any node named title then
+	// returns the data of that node's first child
+	var f func(*html.Node) (string, bool)
+	f = func(n *html.Node) (string, bool) {
+		// If it's an element and it's a title node, look for a child
+		if n.Type == html.ElementNode && n.Data == "title" {
+			if n.FirstChild != nil {
+				t := n.FirstChild.Data
+				t = titleRegex.ReplaceAllString(t, " ")
+				t = strings.TrimSpace(t)
+
+				if t != "" {
+					return t, true
+				} else {
+					return "", false
+				}
+			}
+		}
+
+		// Loop through all nodes and try recursing
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if str, ok := f(c); ok {
+				return str, true
+			}
+		}
+
+		return "", false
+	}
+	if str, ok := f(z); ok {
+		return str
+	} else {
+		return ""
 	}
 }
 
