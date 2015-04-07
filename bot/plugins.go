387de320b6dc1e4181734ada4b355ef985a03763
Kaleb Elwert <kelwert@atlassian.com>
2015-04-07 11:31:29 -0700

    Remove file which was added by accident

--- bot/plugins.go
+++
@@ -1,8 +0,0 @@
-package bot
-
-// A plugin is fairly simple in that it only needs to have a method that will
-// register itself with the muxes inside the bot that it needs, however the
-// actual plugin can be as simple or as complex as needed.
-type Plugin interface {
-	Register(b *Bot) error
-}
