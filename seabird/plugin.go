942c3b1285652861be5aaaddb333dd4552561a21
Kaleb Elwert <belak@coded.io>
2016-08-03 16:34:44 -0700

    Change seabird.RegisterPlugin to panic on error

--- seabird/plugin.go
+++ seabird/plugin.go
@@ -4,7 +4,11 @@ import "github.com/belak/go-plugin"
 
 var plugins = plugin.NewRegistry()
 
-// RegisterPlugin registers a PluginFactory for a given name.
-func RegisterPlugin(name string, factory interface{}) error {
-	return plugins.Register(name, factory)
+// RegisterPlugin registers a PluginFactory for a given name. It will
+// panic if multiple plugins are registered with the same name.
+func RegisterPlugin(name string, factory interface{}) {
+	err := plugins.Register(name, factory)
+	if err != nil {
+		panic(err.Error())
+	}
 }
