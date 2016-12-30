b705ec6ddc31f833665b175605f1a99b7f9762f5
Kaleb Elwert <belak@coded.io>
2016-12-29 16:25:39 -0800

    Improve logging

--- plugins/channel_track.go
+++ plugins/channel_track.go
@@ -12,8 +12,6 @@ import (
 
 // TODO: Figure out what to do with dead sessions in other
 // plugins... maybe a session removal callback.
-//
-// TODO: Improve logging
 
 func init() {
 	seabird.RegisterPlugin("channel_track", newChannelTracker)
@@ -60,6 +58,11 @@ func (u *User) InChannel(channel string) bool {
 type ChannelTracker struct {
 	isupport *ISupportPlugin
 
+	// Notes for internal fields. Be very careful when modifying the
+	// state. Because we control all of this, it is valid to make the
+	// assumption that if a user is in p.uuids, it will be possible to
+	// find them in p.users.
+
 	// Channels can't be renamed, so it's just a mapping of name to
 	// channel object.
 	channels map[string]*Channel
@@ -87,7 +90,10 @@ func newChannelTracker(bm *seabird.BasicMux, isupport *ISupportPlugin) *ChannelT
 	bm.Event("NICK", p.nickCallback)
 
 	bm.Event("353", p.namesCallback)
-	bm.Event("366", p.endOfNamesCallback)
+	// We don't need this for anything currently, so it's being
+	// disabled but left around.
+	//
+	// bm.Event("366", p.endOfNamesCallback)
 
 	return p
 }
@@ -144,7 +150,7 @@ func (p *ChannelTracker) joinCallback(b *seabird.Bot, m *irc.Message) {
 
 	p.addUserToChannel(b, user, channel)
 
-	fmt.Printf("%s (%s) joined %s\n", user, p.uuids[user], channel)
+	//fmt.Printf("%s (%s) joined %s\n", user, p.uuids[user], channel)
 }
 
 func (p *ChannelTracker) partCallback(b *seabird.Bot, m *irc.Message) {
@@ -153,17 +159,17 @@ func (p *ChannelTracker) partCallback(b *seabird.Bot, m *irc.Message) {
 
 	p.removeUserFromChannel(b, user, channel)
 
-	fmt.Printf("%s (%s) left %s\n", user, p.uuids[user], channel)
+	//fmt.Printf("%s (%s) left %s\n", user, p.uuids[user], channel)
 }
 
 func (p *ChannelTracker) kickCallback(b *seabird.Bot, m *irc.Message) {
-	actor := m.Prefix.Name
+	//actor := m.Prefix.Name
 	user := m.Params[1]
 	channel := m.Params[0]
 
 	p.removeUserFromChannel(b, user, channel)
 
-	fmt.Printf("%s (%s) kicked %s (%s) from %s\n", actor, p.uuids[actor], user, p.uuids[user], channel)
+	//fmt.Printf("%s (%s) kicked %s (%s) from %s\n", actor, p.uuids[actor], user, p.uuids[user], channel)
 }
 
 func (p *ChannelTracker) quitCallback(b *seabird.Bot, m *irc.Message) {
@@ -171,7 +177,7 @@ func (p *ChannelTracker) quitCallback(b *seabird.Bot, m *irc.Message) {
 
 	p.removeUser(b, user)
 
-	fmt.Printf("%s (%s) quit\n", user, p.uuids[user])
+	//fmt.Printf("%s (%s) quit\n", user, p.uuids[user])
 }
 
 func (p *ChannelTracker) nickCallback(b *seabird.Bot, m *irc.Message) {
@@ -180,7 +186,7 @@ func (p *ChannelTracker) nickCallback(b *seabird.Bot, m *irc.Message) {
 
 	p.renameUser(b, oldUser, newUser)
 
-	fmt.Printf("%s (%s) changed their name to %s\n", oldUser, p.uuids[newUser], newUser)
+	//fmt.Printf("%s (%s) changed their name to %s\n", oldUser, p.uuids[newUser], newUser)
 }
 
 func (p *ChannelTracker) namesCallback(b *seabird.Bot, m *irc.Message) {
@@ -210,7 +216,7 @@ func (p *ChannelTracker) namesCallback(b *seabird.Bot, m *irc.Message) {
 
 		p.addUserToChannel(b, user, channel)
 
-		fmt.Printf("%s (%s) is in channel %s\n", user, p.uuids[user], channel)
+		//fmt.Printf("%s (%s) is in channel %s\n", user, p.uuids[user], channel)
 	}
 }
 
@@ -223,7 +229,10 @@ func (p *ChannelTracker) endOfNamesCallback(b *seabird.Bot, m *irc.Message) {
 // Implementation below
 
 func (p *ChannelTracker) addUserToChannel(b *seabird.Bot, user, channel string) {
-	logger := b.GetLogger().WithField("channel", channel)
+	logger := b.GetLogger().WithFields(logrus.Fields{
+		"channel": channel,
+		"user":    user,
+	})
 
 	// If the current user is joining a channel, we need to add it
 	// before adding our user.
@@ -234,34 +243,35 @@ func (p *ChannelTracker) addUserToChannel(b *seabird.Bot, user, channel string) 
 	// If we're not in this channel, issue a warning and bail.
 	c, ok := p.channels[channel]
 	if !ok {
-		logger.Warn("Bot not in channel")
+		logger.Warn("Error adding user: bot not in channel")
 		return
 	}
 
-	// If there's no mapping, add it
+	// If there's no mapping, add it and create the user.
 	userUUID, ok := p.uuids[user]
 	if !ok {
 		userUUID = uuid.NewV4().String()
 		p.uuids[user] = userUUID
-	}
-
-	// Add the user if they don't exist.
-	u, ok := p.users[userUUID]
-	if !ok {
-		u = &User{
+		p.users[userUUID] = &User{
 			Nick:     user,
 			UUID:     userUUID,
 			channels: make(map[string]bool),
 		}
-		p.users[userUUID] = u
 	}
 
+	logger = logger.WithField("userUUID", userUUID)
+
+	u := p.users[userUUID]
+
 	if _, ok := u.channels[channel]; ok {
-		logger.Warn("User is already in channel")
+		logger.Warn("User already in channel")
+		return
 	}
 
 	u.channels[channel] = true
 	c.users[userUUID] = true
+
+	logger.Info("User added to channel")
 }
 
 func (p *ChannelTracker) removeUserFromChannel(b *seabird.Bot, user, channel string) {
@@ -276,43 +286,51 @@ func (p *ChannelTracker) removeUserFromChannel(b *seabird.Bot, user, channel str
 			return
 		}
 
+		logger = logger.WithField("userUUID", userUUID)
+
 		u := p.users[userUUID]
 		if _, ok := u.channels[channel]; !ok {
-			logger.Warn("Not in channel")
+			logger.Warn("Can only remove users from channels we are in")
 		} else {
 			delete(u.channels, channel)
 		}
 
+		logger.Info("Removing user from channel")
+
 		if len(u.channels) == 0 {
 			p.removeUser(b, user)
 		}
+
+		logger.Info("Removed user from channel")
 	}
 }
 
 func (p *ChannelTracker) addChannel(b *seabird.Bot, channel string) {
-	logger := b.GetLogger()
+	logger := b.GetLogger().WithField("channel", channel)
 
-	// TODO: This can get called if we're already in the channel. This
-	// will cause some invalid warning calls.
-
-	// Woo! We joined a channel!
 	if _, ok := p.channels[channel]; ok {
-		logger.WithField("channel", channel).Warn("Already in channel")
+		logger.Warn("Already in channel")
+		return
 	}
 
 	p.channels[channel] = &Channel{
 		users: make(map[string]bool),
 	}
+
+	logger.Info("Added channel")
 }
 
 func (p *ChannelTracker) removeChannel(b *seabird.Bot, channel string) {
-	logger := b.GetLogger()
+	logger := b.GetLogger().WithField("channel", channel)
 
 	c, ok := p.channels[channel]
 	if !ok {
-		logger.Warn("Not in channel")
+		logger.Warn("Can only remove channels we are in")
 		return
 	}
+
+	// We only do this for channels because it can have side effects
+	logger.Info("Removing channel")
 
 	// Remove all users currently in this channel from this channel.
 	for userUUID := range c.users {
@@ -330,10 +348,12 @@ func (p *ChannelTracker) removeChannel(b *seabird.Bot, channel string) {
 
 	// Remove the channel from tracking
 	delete(p.channels, channel)
+
+	logger.Info("Removed channel")
 }
 
 func (p *ChannelTracker) removeUser(b *seabird.Bot, user string) {
-	logger := b.GetLogger()
+	logger := b.GetLogger().WithField("user", user)
 
 	userUUID, ok := p.uuids[user]
 	if !ok {
@@ -353,18 +373,23 @@ func (p *ChannelTracker) removeUser(b *seabird.Bot, user string) {
 	// Now that the User is empty, delete all internal traces.
 	delete(p.uuids, user)
 	delete(p.users, userUUID)
+
+	logger.Info("Removed user")
 }
 
 func (p *ChannelTracker) renameUser(b *seabird.Bot, oldNick, newNick string) {
+	logger := b.GetLogger().WithFields(logrus.Fields{
+		"oldNick": oldNick,
+		"newNick": newNick,
+	})
+
 	userUUID, ok := p.uuids[oldNick]
 	if !ok {
-		logger := b.GetLogger()
-		logger.WithFields(logrus.Fields{
-			"oldNick": oldNick,
-			"newNick": newNick,
-		}).Warn("Can't rename user that doesn't exist")
+		logger.Warn("Can't rename user that doesn't exist")
 		return
 	}
+
+	logger = logger.WithField("userUUID", userUUID)
 
 	// Rename the user object
 	u := p.users[userUUID]
@@ -373,4 +398,6 @@ func (p *ChannelTracker) renameUser(b *seabird.Bot, oldNick, newNick string) {
 	// Swap where the UUID points to
 	delete(p.uuids, oldNick)
 	p.uuids[newNick] = userUUID
+
+	logger.Info("Renamed user")
 }
