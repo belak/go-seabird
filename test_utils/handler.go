c67ec3b1262ccddc311ce0d082bb1896d1a47537
Kaleb Elwert <belak@coded.io>
2017-01-07 16:57:44 -0800

    Add some useful test utils

---
+++ test_utils/handler.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"sync"
+
+	seabird "github.com/belak/go-seabird"
+	"github.com/go-irc/irc"
+)
+
+// TestHandler is meant to be inserted as a Handler somewhere to
+// capture all messages which are sent.
+type TestHandler struct {
+	messages []*irc.Message
+	lock     sync.Mutex
+}
+
+// Handle implements the Handler interface
+func (th *TestHandler) Handle(b *seabird.Bot, m *irc.Message) {
+	th.lock.Lock()
+	defer th.lock.Unlock()
+
+	th.messages = append(th.messages, m)
+}
+
+// Messages will return all the messages that were passed to this
+// handler.
+func (th *TestHandler) Messages() []*irc.Message {
+	th.lock.Lock()
+	defer th.lock.Unlock()
+
+	ret := th.messages
+	th.messages = nil
+	return ret
+}
