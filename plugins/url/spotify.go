956e4614d2c23e1571ca3bc2b6e2ed1874d30237
Kaleb Elwert <belak@coded.io>
2018-05-15 11:57:56 -0700

    Add artist, track and playlist URL matchers for Spotify

--- plugins/url/spotify.go
+++ plugins/url/spotify.go
@@ -31,21 +31,36 @@ type spotifyProvider struct {
 var spotifyPrefix = "[Spotify]"
 
 type spotifyMatch struct {
-	regex    *regexp.Regexp
-	template *template.Template
-	lookup   func(*spotifyProvider, *logrus.Entry, spotify.ID) interface{}
+	matchCount int
+	regex      *regexp.Regexp
+	template   *template.Template
+	lookup     func(*spotifyProvider, *logrus.Entry, []string) interface{}
 }
 
 var spotifyMatchers = []spotifyMatch{
 	{
-		regex: regexp.MustCompile(`^/album/(.+)$`),
+		matchCount: 1,
+		regex:      regexp.MustCompile(`^/artist/(.+)$`),
+		template:   TemplateMustCompile("spotifyArtist", `{{- .Name -}}`),
+		lookup: func(s *spotifyProvider, logger *logrus.Entry, matches []string) interface{} {
+			artist, err := s.api.GetArtist(spotify.ID(matches[0]))
+			if err != nil {
+				logger.WithError(err).Error("Failed to get artist info from Spotify")
+				return nil
+			}
+			return artist
+		},
+	},
+	{
+		matchCount: 1,
+		regex:      regexp.MustCompile(`^/album/(.+)$`),
 		template: TemplateMustCompile("spotifyAlbum", `
 			{{- .Name }} by
 			{{- range $index, $element := .Artists }}
 			{{- if $index }},{{ end }} {{ $element.Name -}}
 			{{- end }}`),
-		lookup: func(s *spotifyProvider, logger *logrus.Entry, id spotify.ID) interface{} {
-			album, err := s.api.GetAlbum(id)
+		lookup: func(s *spotifyProvider, logger *logrus.Entry, matches []string) interface{} {
+			album, err := s.api.GetAlbum(spotify.ID(matches[0]))
 			if err != nil {
 				logger.WithError(err).Error("Failed to get album info from Spotify")
 				return nil
@@ -53,9 +68,38 @@ var spotifyMatchers = []spotifyMatch{
 			return album
 		},
 	},
+	{
+		matchCount: 1,
+		regex:      regexp.MustCompile(`^/track/(.+)$`),
+		template: TemplateMustCompile("spotifyTrack", `
+			"{{ .Name }}" from {{ .Album.Name }} by
+			{{- range $index, $element := .Artists }}
+			{{- if $index }},{{ end }} {{ $element.Name }}
+			{{- end }}`),
+		lookup: func(s *spotifyProvider, logger *logrus.Entry, matches []string) interface{} {
+			track, err := s.api.GetTrack(spotify.ID(matches[0]))
+			if err != nil {
+				logger.WithError(err).Error("Failed to get track info from Spotify")
+				return nil
+			}
+			return track
+		},
+	},
+	{
+		matchCount: 2,
+		regex:      regexp.MustCompile(`^/user/([^/]*)/playlist/([^/]*)$`),
+		template: TemplateMustCompile("spotifyPlaylist", `
+			"{{- .Name }}" playlist by {{ .Owner.DisplayName }}`),
+		lookup: func(s *spotifyProvider, logger *logrus.Entry, matches []string) interface{} {
+			playlist, err := s.api.GetPlaylist(matches[0], spotify.ID(matches[1]))
+			if err != nil {
+				logger.WithError(err).Error("Failed to get track info from Spotify")
+				return nil
+			}
+			return playlist
+		},
+	},
 }
-
-var spotifyAlbumRegex = regexp.MustCompile(`^/album/(.+)$`)
 
 func newSpotifyProvider(b *seabird.Bot, urlPlugin *Plugin) error {
 	s := &spotifyProvider{}
@@ -92,11 +136,11 @@ func (s *spotifyProvider) Handle(b *seabird.Bot, m *irc.Message, u *url.URL) boo
 		}
 
 		matches := matcher.regex.FindStringSubmatch(u.Path)
-		if len(matches) != 2 {
+		if len(matches) != matcher.matchCount+1 {
 			return false
 		}
 
-		data := matcher.lookup(s, logger, spotify.ID(matches[1]))
+		data := matcher.lookup(s, logger, matches[1:])
 		if data == nil {
 			return false
 		}
